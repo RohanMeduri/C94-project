2b31874f224dc0d689c2c5306ca88c9ba7d528d2
RohanMeduri <86210535+RohanMeduri@users.noreply.github.com>
2022-11-06 18:51:25 -0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEcSiuaCdse4CA8HDL_PntBp0w2MjNSjA",
+    authDomain: "c94-project-63ba9.firebaseapp.com",
+    projectId: "c94-project-63ba9",
+    storageBucket: "c94-project-63ba9.appspot.com",
+    messagingSenderId: "90203216180",
+    appId: "1:90203216180:web:1b871455d430118dd82d61"
+  };
